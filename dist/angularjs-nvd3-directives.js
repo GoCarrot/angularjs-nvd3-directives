08ed1840beef4f4f841aef1cd208ff4a49de860c
Alex Scarborough <alex@teak.io>
2020-10-27 16:28:16 -0700

    Update build.

--- dist/angularjs-nvd3-directives.js
+++ dist/angularjs-nvd3-directives.js
@@ -2646,10 +2646,6 @@
           alignvalue: '@',
           rightalignvalue: '@',
           nodata: '@',
-          status: '&',
-          showFirstPoint: '@',
-          showStatusChange: '@',
-          interactive: '@',
           callback: '&',
           xtickformat: '&',
           ytickformat: '&',
@@ -2686,9 +2682,7 @@
                     return d.x;
                   } : scope.x() ).y( attrs.y === undefined ? function ( d ) {
                     return d.y;
-                  } : scope.y() ).status( attrs.status === undefined ? function ( d ) {
-                    return d.status;
-                  } : scope.status() ).xTickFormat( attrs.xtickformat === undefined ? d3.format( ',r' ) : scope.xtickformat() ).yTickFormat( attrs.ytickformat === undefined ? d3.format( ',.2f' ) : scope.ytickformat() ).color( attrs.color === undefined ? nv.utils.getColor( [ '#000' ] ) : scope.color() ).showLastValue( attrs.showvalue === undefined ? true : attrs.showvalue === 'true' ).alignValue( attrs.alignvalue === undefined ? true : attrs.alignvalue === 'true' ).rightAlignValue( attrs.rightalignvalue === undefined ? false : attrs.rightalignvalue === 'true' ).noData( attrs.nodata === undefined ? 'No Data Available.' : scope.nodata ).interactive( attrs.interactive === undefined ? false : attrs.interactive === 'true' ).showFirstPoint( attrs.showFirstPoint === undefined ? false : attrs.showFirstPoint === 'true' ).showStatusChange( attrs.showStatusChange === undefined ? false : attrs.showStatusChange === 'true' );
+                  } : scope.y() ).xTickFormat( attrs.xtickformat === undefined ? d3.format( ',r' ) : scope.xtickformat() ).yTickFormat( attrs.ytickformat === undefined ? d3.format( ',.2f' ) : scope.ytickformat() ).color( attrs.color === undefined ? nv.utils.getColor( [ '#000' ] ) : scope.color() ).showValue( attrs.showvalue === undefined ? true : attrs.showvalue === 'true' ).alignValue( attrs.alignvalue === undefined ? true : attrs.alignvalue === 'true' ).rightAlignValue( attrs.rightalignvalue === undefined ? false : attrs.rightalignvalue === 'true' ).noData( attrs.nodata === undefined ? 'No Data Available.' : scope.nodata );
                   if ( attrs.xScale ) {
                     chart.xScale( scope.xScale() );
                   }
